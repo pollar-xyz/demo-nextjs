f274b00c24727179f51153cf7d1490224fe82fef
Oscar Gauss Carvajal Yucra <dev@oscargauss.com>
2026-02-26 04:15:35 -0400

    upd @pollar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-import { PollarProvider, WalletButton } from '@pollar/react';
+import { PollarProvider, usePollar, WalletButton } from '@pollar/react';
 import Image from 'next/image';
 import '@pollar/react/styles.css';
+
+const Log = () => {
+  const pollar = usePollar();
+  console.log({ pollar });
+  return null;
+};
 
 export default function Home() {
   return (
     <PollarProvider
       config={{
-        baseUrl: 'https://sdk.pollar.xyz',
-        apiKey: 'cmlq7p1800002mwidnveszrsz',
+        apiKey: 'cmm2qa4d30001gkid7myxvd1v',
       }}
     >
+      <Log />
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <Image
@@ -23,7 +29,7 @@ export default function Home() {
             priority
           />
           <WalletButton />
-          
+
           <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
             <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
               To get started, edit the page.tsx file.
