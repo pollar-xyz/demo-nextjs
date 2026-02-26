d8485d3e019b787625b7ad74410c534f370fdc75
Oscar Gauss Carvajal Yucra <dev@oscargauss.com>
2026-02-26 12:54:28 -0400

    upd pollar to v0.3.4

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { PollarProvider, usePollar, WalletButton } from '@pollar/react';
+import { PollarProvider, WalletButton } from '@pollar/react';
 import Image from 'next/image';
 import '@pollar/react/styles.css';
-
-const Log = () => {
-  const pollar = usePollar();
-  console.log({ pollar });
-  return null;
-};
 
 export default function Home() {
   return (
@@ -17,7 +11,6 @@ export default function Home() {
         apiKey: 'cmm2qa4d30001gkid7myxvd1v',
       }}
     >
-      <Log />
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
           <Image
